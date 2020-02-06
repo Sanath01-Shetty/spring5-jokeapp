fd5e32e443882e094cb23752c1bdc7a1018a2dc7
Sanath01_Shetty <shetty.sanath1911@gmail.com>
2020-02-06 22:33:40 +0530

    Create Project

---
+++ src/main/java/com/jokeapp/spring5/spring5jokeapp/Spring5JokeappApplication.java
@@ -0,0 +1,13 @@
+package com.jokeapp.spring5.spring5jokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JokeappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5JokeappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jokeapp/spring5/spring5jokeapp/Spring5JokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jokeapp.spring5.spring5jokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
